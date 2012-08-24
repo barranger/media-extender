08c72da29f331eb22b282bbcf80263628193d327
Barranger Ridler <barranger@gmail.com>
2012-08-24 13:59:30 -0400

    Added mustache.js support

--- processors.js
+++ processors.js
@@ -1,66 +1,68 @@
 var processorSetup = function($, processors) {
 	//Adding General Image processor
 	processors.push(function(url, callback){
-		var directImageRegex =  /(https?:\/\/.*?\.(?:png|jpe?g|gif))/i;
-		var imgHtml = "<div class='inserted-image' style='margin-top: 12px;'><a href='~~~' target='_blank'><img src='~~~' style='max-width: 435px; max-height: 375px;' /></a></div>";
-		if( url.search(directImageRegex) != -1 ) {
-			callback(imgHtml.replace(/~~~/g, url.match(directImageRegex)[0]));
+		var reg  =  /(https?:\/\/.*?\.(?:png|jpe?g|gif))/i;
+		var html = "<div class='inserted-image' style='margin-top: 12px;'><a href='{{imgUrl}}' target='_blank'><img src='{{imgUrl}}' style='max-width: 435px; max-height: 375px;' /></a></div>";
+		if( url.search(reg) != -1 ) {
+			callback( Mustache.render(html, { imgUrl: url.match(reg)[0] }));
 		}
 	});
 
 	//Adding Youtube embeder processor
 	processors.push(function(url, callback) {
-		var regularYT = /https?:\/\/www.youtube.com\/watch.*?(\?|\&)v=.*/i;
-		var shortYT = /https?:\/\/youtu.be\/.*/i;
-		var vidHtml = '<div class="inserted-video" style="margin-top: 12px;"><object width="425" height="350"><param name="movie" value="http://youtube.com/v/~~~"><embed src="https://youtube.com/v/~~~" type="application/x-shockwave-flash" width="425" height="350"></object></div>'
-		
-		if(url.search(regularYT) != -1 ) {
-			var vidId = url.substring(url.indexOf("v=") + 2);
-			callback(vidHtml.replace(/~~~/g, vidId));
-		}
+		var reg = /https?:\/\/www.youtube.com\/watch.*?(\?|\&)v=.*/i;
+		var sreg = /https?:\/\/youtu.be\/.*/i;
+		var html = '<div class="inserted-video" style="margin-top: 12px;"><object width="425" height="350"><param name="movie" value="http://youtube.com/v/{{vidId}}"><embed src="https://youtube.com/v/{{vidId}}" type="application/x-shockwave-flash" width="425" height="350"></object></div>'
+		var vidId = null;
 
-		if(url.search(shortYT) != -1 ) {
-			var vidId = url.substring(url.lastIndexOf("/") + 1);
-			callback(vidHtml.replace(/~~~/g, vidId));
-		}
+		if(url.search(reg) != -1 )
+			vidId = url.substring(url.indexOf("v=") + 2);
+
+		if(url.search(sreg) != -1 ) 
+			vidId = url.substring(url.lastIndexOf("/") + 1);
+
+		if(vidId)
+			callback(Mustache.render(html, {vidId:vidId}));
+
 	});
 
 	//Adding Instagram
 	processors.push(function(url, callback){
-		var imgHtml = "<div class='inserted-image' style='margin-top: 12px;'><a href='~~~' target='_blank'><img src='https://instagr.am/p/~ID~media/' style='max-width: 435px; max-height: 375px;' /></a></div>";
-		var regularIG = /https?:\/\/(instagram.com|instagr.am)\/p\/.*/i;
+		var html = "<div class='inserted-image' style='margin-top: 12px;'><a href='{{url}}' target='_blank'><img src='https://instagr.am/p/{{vidId}}media/' style='max-width: 435px; max-height: 375px;' /></a></div>";
+		var reg  = /https?:\/\/(instagram.com|instagr.am)\/p\/.*/i;
 
-		if(url.search(regularIG) != -1 ) {
+		if(url.search(reg) != -1 ) {
 			var igId = url.substring(url.indexOf("/p/") + 3 );
 			if(igId.substr(-1) !== "/") {
 				igId = igId + "/";
 			}
-			callback(imgHtml.replace("~~~", url).replace("~ID~", igId));
+			callback(Mustache.render(html, {url:url,vidId:igId }));
 		}
 	});
 
 	//Adding Imgur (where it doesn't contain .jpg)
 	processors.push(function(url, callback){
-		var imgHtml = "<div class='inserted-image' style='margin-top: 12px;'><a href='http://imgur.com/~~~' target='_blank'><img src='http://i.imgur.com/~~~.jpg' style='max-width: 435px; max-height: 375px;' /></a></div>";
-		var regularIU = /https?:\/\/imgur.com\/.*/i;
+		var html = "<div class='inserted-image' style='margin-top: 12px;'><a href='http://imgur.com/{{id}}' target='_blank'><img src='http://i.imgur.com/{{id}}.jpg' style='max-width: 435px; max-height: 375px;' /></a></div>";
+		var reg  = /https?:\/\/imgur.com\/.*/i;
 
-		if(url.search(regularIU) != -1 ) {
+		if(url.search(reg) != -1 ) {
 			var iuId = url.substring(url.lastIndexOf("/") + 1 );
-			callback(imgHtml.replace(/~~~/g, iuId));
+			callback(Mustache.render(html, {id: iuId }));
 		}
 	});
 
 	//Adding Tweet support
 	processors.push(function(url, callback) {
-		var regularTw = /https?:\/\/twitter.com\/.*?\/status/i;
+		var html = "<div style='margin-top: 12px;'>{{{html}}}</div>";
+		var reg = /https?:\/\/twitter.com\/.*?\/status/i;
 
-		if(url.search(regularTw) != -1) {
+		if(url.search(reg) != -1) {
 			var tweetId = url.substring(url.lastIndexOf("/") + 1);
 			
 			$.ajax({
 		        url: "https://api.twitter.com/1/statuses/oembed.json?id=" + tweetId + "&align=center",
 		        success: function(data) {
-		            callback('<div style="margin-top: 12px;"">' + data.html + '</div>');
+		            callback(Mustache.render(html, data));
 		        }
 		    });
 		}
@@ -68,13 +70,14 @@ var processorSetup = function($, processors) {
 
 	//Adding Vimeo support
 	processors.push(function(url, callback) {
+		var html = "<div style='margin-top: 12px;'>{{{html}}}</div>";
 		var reg = /https?:\/\/vimeo.com\//i;
 
 		if(url.search(reg) != -1) {
 			$.ajax({
 		        url: "https://vimeo.com/api/oembed.json?maxwidth=435&url=" + url,
 		        success: function(data) {
-		            callback('<div style="margin-top: 12px;"">' + data.html + '</div>');
+		            callback(Mustache.render(html, data));
 		        }
 		    });
 		}
